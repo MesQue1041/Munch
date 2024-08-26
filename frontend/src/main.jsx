ed5f2fabf90876f6b73b01af657244d3be875d47
MesQue1041 <dovebookt@gmail.com>
2024-08-26 21:01:13 +0530

    Item list done

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {BrowserRouter} from 'react-router-dom'
+import StoreContextProvider from './context/StoreContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+    <StoreContextProvider>    {/*This setup allows you to provide the context to the entire app, making it accessible to all components that are wrapped by the provider*/}
+        <App />
+    </StoreContextProvider>
+    </BrowserRouter>
 )
