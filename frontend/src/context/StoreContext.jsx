1f0b78819a8a887f2dbf01405aa18cad497a061f
MesQue1041 <dovebookt@gmail.com>
2024-08-26 23:37:58 +0530

    Home page done

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/assets";
 
 
@@ -15,11 +15,22 @@ const StoreContextProvider = (props) => {
         else {
             setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
         }
-
     }
 
+    const removeFromCart = (itemId) => {
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+    }
+
+    useEffect(()=> {
+        console.log(cartItems);
+    },[cartItems])
+
     const contextValue = {
-        food_list
+        food_list,
+        cartItems,
+        setCartItems,
+        addToCart,
+        removeFromCart
     }
     return(
         <StoreContext.Provider value={contextValue}>
