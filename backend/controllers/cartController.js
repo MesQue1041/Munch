6bd8f4f9776ce66908de0ad2c534a8e0fee2eef7
MesQue1041 <dovebookt@gmail.com>
2024-08-28 18:11:12 +0530

    Add to cart functionality donw

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -2,7 +2,22 @@ import userModel from "../models/userModel.js"
 
 // Add items to user Cart
 const addToCart = async (req,res) => {
-
+    try {
+        let userData = await userModel.findOne({_id:req.body.userId});
+        let cartData = await userData.cartData;
+        if(!cartData[req.body.itemId])
+        {
+            cartData[req.body.itemId] = 1;
+        }
+        else {
+            cartData[req.body.itemId] += 1;
+        }
+        await userModel.findByIdAndUpdate(req.body.userId,{cartData})
+        res.json({success:true,message:"Added to Cart"})
+    } catch(error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
 }
 
 
