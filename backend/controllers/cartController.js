1ca79195ea6e676329ca073d6ac24ecad395743c
MesQue1041 <dovebookt@gmail.com>
2024-08-28 16:40:18 +0530

    Created seperate controller and route for cart

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,21 @@
+import userModel from "../models/userModel.js"
+
+// Add items to user Cart
+const addToCart = async (req,res) => {
+
+}
+
+
+// Remove items from user Cart
+const removeFromCart = async (req,res) => {
+
+}
+
+
+// Fetch user Cart data
+const getCart = async (req,res) => {
+
+}
+
+
+export {addToCart,removeFromCart,getCart}
--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useState } from "react";
-import { food_list } from "../assets/assets";
+import axios from "axios"
 
 
 export const StoreContext = createContext(null)
@@ -8,7 +8,8 @@ const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
     const url = "http://localhost:4000"
-    const [token,setToken] = useState("") 
+    const [token,setToken] = useState("");
+    const [food_list,setFootList] = useState([]); 
 
     const addToCart = (itemId) => {
         if (!cartItems[itemId]) {
@@ -35,11 +36,19 @@ const StoreContextProvider = (props) => {
         return totalAmount;
     }
 
-    useEffect(()=>{
-        if (localStorage.getItem("token")){
-            setToken(localStorage.getItem("token"));
-        }
+    const fetchFoodList = async () => {
+        const response = await axios.get(url+"/api/food/list")
+        setFootList(response.data.data)
+    }
 
+    useEffect(()=>{
+        async function loadData() {
+            await fetchFoodList();
+            if (localStorage.getItem("token")){
+                setToken(localStorage.getItem("token"));
+            }
+        }
+        loadData();
     }, [])
 
 
