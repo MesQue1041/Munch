f478bb2df3b696581fda83be802066782b9fe221
MesQue1041 <dovebookt@gmail.com>
2024-08-28 16:25:00 +0530

    Login and Logout works

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -7,6 +7,8 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
+    const url = "http://localhost:4000"
+    const [token,setToken] = useState("") 
 
     const addToCart = (itemId) => {
         if (!cartItems[itemId]) {
@@ -33,6 +35,13 @@ const StoreContextProvider = (props) => {
         return totalAmount;
     }
 
+    useEffect(()=>{
+        if (localStorage.getItem("token")){
+            setToken(localStorage.getItem("token"));
+        }
+
+    }, [])
+
 
 
     const contextValue = {
@@ -41,7 +50,10 @@ const StoreContextProvider = (props) => {
         setCartItems,
         addToCart,
         removeFromCart,
-        getTotalCartAmount
+        getTotalCartAmount,
+        url,
+        token,
+        setToken
     }
     return (
         <StoreContext.Provider value={contextValue}>
